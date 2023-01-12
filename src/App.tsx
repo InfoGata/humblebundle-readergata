c97cc23a32903d395be273dc3cc35008650360c6
Elijah Green <elijah@infogata.com>
2023-01-12 11:50:17 -0800

    Add instructions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Box, CssBaseline, TextField, Button } from "@mui/material";
+import {
+  Box,
+  CssBaseline,
+  TextField,
+  Button,
+  Typography,
+  Stack,
+} from "@mui/material";
 import { FunctionComponent } from "preact";
 import { useEffect, useState } from "preact/hooks";
 import { MessageType, UiMessageType } from "./shared";
@@ -34,17 +41,62 @@ const App: FunctionComponent = () => {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <TextField
-        label="_simpleauth_sess Cookie"
-        value={simpleAuth}
-        onChange={(e) => {
-          const value = e.currentTarget.value;
-          setSimpleAuth(value);
-        }}
-      />
-      <Button variant="contained" onClick={saveCookie}>
-        Save
-      </Button>
+      <Stack spacing={2}>
+        <Typography>Instructions:</Typography>
+        <ol>
+          <li>
+            Browse to{" "}
+            <a href="https://www.humblebundle.com/" target="_blank">
+              https://www.humblebundle.com/
+            </a>{" "}
+            and login.
+          </li>
+          <li>
+            <p>Find the value of the cookie _simpleauth_sess.</p>
+            <p>
+              Firefox: Open the{" "}
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Tools/Storage_Inspector"
+                target="_blank"
+              >
+                Storage Inspector
+              </a>
+            </p>
+            <p>
+              Chrome: Open the{" "}
+              <a
+                href="https://developer.chrome.com/devtools/docs/resource-panel#cookies"
+                target="_blank"
+              >
+                Resource Panel
+              </a>
+            </p>
+            <p>
+              Safari: Open the Storage Tab of the{" "}
+              <a
+                href="https://developer.apple.com/safari/tools/"
+                target="_blank"
+              >
+                Web Inspector
+              </a>{" "}
+              (Context menu on the page, click Inspect page) click the Storage
+              tab, select Cookies on the sidebar
+            </p>
+          </li>
+          <li>Paste into _simpleauth_sess Cookie text field and click Save</li>
+        </ol>
+        <TextField
+          label="_simpleauth_sess Cookie"
+          value={simpleAuth}
+          onChange={(e) => {
+            const value = e.currentTarget.value;
+            setSimpleAuth(value);
+          }}
+        />
+        <Button variant="contained" onClick={saveCookie}>
+          Save
+        </Button>
+      </Stack>
     </Box>
   );
 };
