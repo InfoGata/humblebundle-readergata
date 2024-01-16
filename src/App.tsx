35f0dfe39701ed7754986338659c92dcad3040ba
Elijah Green <elijah@infogata.com>
2024-01-16 00:40:17 -0800

    Use solidjs and solid-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import {
-  Box,
-  CssBaseline,
-  Typography,
-  Stack,
-  Button,
-  TextField,
-} from "@mui/material";
-import { FunctionComponent } from "preact";
-import { useEffect, useState } from "preact/hooks";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 import { MessageType, UiMessageType } from "./shared";
+import { createEffect, createSignal } from "solid-js";
 
 const sendUiMessage = (message: UiMessageType) => {
   parent.postMessage(message, "*");
 };
 
-const App: FunctionComponent = () => {
-  const [extensionInstalled, setExtensionInstalled] = useState(true);
-  const [simpleAuth, setSimpleAuth] = useState("");
+const App = () => {
+  const [extensionInstalled, setExtensionInstalled] = createSignal(true);
+  const [simpleAuth, setSimpleAuth] = createSignal("");
 
-  useEffect(() => {
+  createEffect(() => {
     const onMessage = (event: MessageEvent<MessageType>) => {
       switch (event.data.type) {
         case "info":
@@ -34,27 +27,26 @@ const App: FunctionComponent = () => {
     window.addEventListener("message", onMessage);
     sendUiMessage({ type: "check-login" });
     return () => window.removeEventListener("message", onMessage);
-  }, []);
+  });
 
   const saveCookie = () => {
-    sendUiMessage({ type: "save", simpleAuth });
+    sendUiMessage({ type: "save", simpleAuth: simpleAuth() });
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <Stack spacing={2}>
-        <Typography>Instructions:</Typography>
-        <ol>
-          <li>
-            Browse to{" "}
-            <a href="https://www.humblebundle.com/" target="_blank">
-              https://www.humblebundle.com/
-            </a>{" "}
-            and login.
-          </li>
-          <li>
-            <p>Find the value of the cookie _simpleauth_sess.</p>
+    <div class="flex flex-col gap-2">
+      <h1>Instructions:</h1>
+      <ol class="pl-8 list-decimal list-inside space-y-1 flex flex-col">
+        <li>
+          Browse to{" "}
+          <a href="https://www.humblebundle.com/" target="_blank">
+            https://www.humblebundle.com/
+          </a>{" "}
+          and login.
+        </li>
+        <li>
+          Find the value of the cookie _simpleauth_sess.
+          <div class="pl-8">
             <p>
               Firefox: Open the{" "}
               <a
@@ -84,34 +76,32 @@ const App: FunctionComponent = () => {
               (Context menu on the page, click Inspect page) click the Storage
               tab, select Cookies on the sidebar
             </p>
-          </li>
-          <li>Paste into _simpleauth_sess Cookie text field and click Save</li>
-          <li>
-            Install the{" "}
-            <a
-              target="_blank"
-              href="https://github.com/InfoGata/infogata-extension"
-            >
-              InfoGata Extension
-            </a>
-          </li>
-          {extensionInstalled && (
-            <li>You should now be able to browse yours books in Library</li>
-          )}
-        </ol>
-        <TextField
-          label="_simpleauth_sess Cookie"
-          value={simpleAuth}
-          onChange={(e) => {
-            const value = e.currentTarget.value;
-            setSimpleAuth(value);
-          }}
-        />
-        <Button variant="contained" onClick={saveCookie}>
-          Save
-        </Button>
-      </Stack>
-    </Box>
+          </div>
+        </li>
+        <li>Paste into _simpleauth_sess Cookie text field and click Save</li>
+        <li>
+          Install the{" "}
+          <a
+            target="_blank"
+            href="https://github.com/InfoGata/infogata-extension"
+          >
+            InfoGata Extension
+          </a>
+        </li>
+        {extensionInstalled() && (
+          <li>You should now be able to browse yours books in Library</li>
+        )}
+      </ol>
+      <Input
+        placeholder="_simpleauth_sess Cookie"
+        value={simpleAuth()}
+        onChange={(e) => {
+          const value = e.currentTarget.value;
+          setSimpleAuth(value);
+        }}
+      />
+      <Button onClick={saveCookie}>Save</Button>
+    </div>
   );
 };
 
